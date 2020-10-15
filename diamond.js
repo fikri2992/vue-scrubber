0fab527dd9ae52499f1bab414e5e2ab8a0e66485
Slegy Sanjaya <slegysanjaya@gmail.com>
2020-10-15 16:38:31 +0700

    add javascript for create diamond

---
+++ diamond.js
@@ -0,0 +1,11 @@
+function diamond(val){
+  var y, w, shape = '';
+
+  for(y = 0; y < val * 2 - 1; y++) {
+    w = y < val ? y : val * 2 - y - 2;
+    shape += Array(val - w).join(' ') + Array(w + 1).join('* ') + '*\n';
+  }
+  document.write('<pre>' + shape + '</pre>');
+}
+
+diamond(7);
